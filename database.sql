7f53db6006b20a8c667b2914af6ee52dab0e0822
vanshdashore <vanshdashoreji1234@gmail.com>
2026-04-05 22:23:02 +0530

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE student_db;
+
+USE student_db;
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    roll_no INT UNIQUE,
+    branch VARCHAR(50),
+    cgpa FLOAT,
+    skills VARCHAR(100)
+);
